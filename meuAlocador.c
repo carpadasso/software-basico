e2e3f257f955a1c048ad62f6736b8b0628d5fc0c
Leonardo Carpwiski <lionheartcarpwiski@gmail.com>
2024-11-12 09:10:14 -0300

    Foundations
    Criação da estrutura do trabalho. Consiste de três arquivos:
    1- exemplo.c: Exemplo de execução que o professor irá utilizar (será modificado);
    2- meuAlocador.c: Funções implementadas do trabalho, que no caso é o alocador de memória;
    3- meuAlocador.h: Biblioteca que será integrada ao trabalho.

---
+++ meuAlocador.c
@@ -0,0 +1,28 @@
+// Implementação da Biblioteca meuAlocador.h
+#include <stdio.h>
+#include "meuAlocador.h"
+
+void iniciaAlocador()
+{
+   
+}
+
+void finalizaAlocador()
+{
+
+}
+
+int liberaMem(void* bloco)
+{
+
+}
+
+void* alocaMem(int num_bytes)
+{
+
+}
+
+void imprimeMapa()
+{
+
+}
