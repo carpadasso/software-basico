02675ed7f9764ec886d289ae33d00afc0ff1afab
Leonardo Carpwiski <lionheartcarpwiski@gmail.com>
2024-11-29 15:12:32 -0300

    Update meuAlocador.s

--- meuAlocador.s
+++ meuAlocador.s
@@ -137,8 +137,7 @@ else_heap_livre:
    subq %rax, %rbx
    movq %rbx, %rax
    addq $1, %rax
-   movq $0, %rdx # ESSA LINHA PODE DAR PROBLEMA?
-   divq $4096
+   shr $12, %rax
    movq %rax, -24(%rbp)
    # brk(topoAtualHeap - num_blocos * 4096)
    shl $12, %rax
@@ -176,6 +175,195 @@ alocaMem:
    # num_blocos   -> -32(%rbp)
    # num_bytes    -> +16(%rbp)
 
+   # melhorBloco   = NULL;
+   movq $0, -24(%rbp)
+   # percorreHeap  = topoInicialHeap + 1;
+   movq TOPO_INICIAL_HEAP, %rax
+   addq $1, %rax
+   movq %rax, -8(%rbp)
+   # while (percorreHeap <= topoAtualHeap)
+   #    ...
+while_percorre:
+   movq -8(%rbp), %rax
+   movq TOPO_ATUAL_HEAP, %rbx
+   cmpq %rbx, %rax
+   jg fim_while_percorre
+   # -if ( *percorreHeap) == 0 )
+   #     ...
+   movq $0, %rbx
+   cmpq %rbx, %rax
+   jne fim_if
+   # -if ( *(percorreHeap + 8) >= num_bytes ) 
+   #    ...
+   movq 16(%rbp), %rbx
+   addq $8, %rax
+   movq (%rax), %rax
+   cmpq %rbx, %rax
+   jl fim_if
+   # -if ( melhorBloco == NULL || 
+   #       *(percorreHeap + 8) < *(melhorBloco + 8) )
+   #     melhorBloco = percorreHeap;
+   movq $0, %rbx
+   movq -24(%rbp), %rax
+   cmpq %rbx, %rax
+   jne if_cond_2
+   movq -8(%rbp), %rax
+   movq %rax, -24(%rbp)
+if_cond_2:
+   movq -8(%rbp), %rax
+   addq $8, %rax
+   movq (%rax), %rax
+   movq -24(%rbp), %rBx
+   addq $8, %rbx
+   movq (%rbx), %rbx
+   cmpq %rbx, %rax
+   jge fim_if
+   movq -8(%rbp), %rax
+   movq %rax, -24(%rbp)
+fim_if:
+   # percorreHeap += *(percorreHeap + 8) + 16;
+   movq -8(%rbp), %rax
+   movq %rax, %rbx
+   addq $8, %rbx
+   movq (%rbx), %rbx
+   addq $16, %rbx
+   addq %rbx, %rax
+   movq %rax, -8(%rbp)
+fim_while_percorre:
+   # -if (melhorBloco != NULL)
+   #     ...
+   movq -24(%rbp), %rax
+   movq $0, %rbx
+   cmpq %rbx, %rax
+   jne else_melhor_bloco
+   # *melhorBloco = 1;
+   movq $1, (%rax)
+   # -if ( *(melhorBloco + 8) > num_bytes + 16 )
+   addq $8, %rax
+   movq (%rax), %rax
+   movq 16(%rbp), %rbx
+   addq $16, %rbx
+   cmpq %rbx, %rax
+   jle fim_if_melhor_bloco
+   # *(melhorBloco + 16 + num_bytes) = 0;
+   movq -24(%rbp), %rax
+   movq 16(%rbp), %rbx
+   addq $16, %rax
+   addq %rbx, %rax
+   movq $0, (%rax)
+   # *(melhorBloco + 24 + num_bytes) = *(melhorBloco + 8) - (num_bytes + 16);
+   movq -24(%rbp), %rax
+   addq $8, %rax
+   movq (%rax), %rax
+   movq 16(%rbp), %rbx
+   addq $16, %rbx
+   subq %rbx, %rax
+   movq -24(%rbp), %rbx
+   movq 16(%rbp), %r10
+   addq $24, %rbx
+   addq %r10, %rbx
+   movq %rax, (%rbx)
+   # *(melhorBloco + 8) = num_bytes;
+   movq 16(%rbp), %rax
+   movq -24(%rbp), %rbx
+   addq $8, %rbx
+   movq %rax, (%rbx)
+   jmp fim_if_melhor_bloco
+else_melhor_bloco:
+   # -if (topoAtualHeap != topoInicialHeap)
+   #    ...
+   movq TOPO_ATUAL_HEAP, %rax
+   movq TOPO_INICIAL_HEAP, %rbx
+   cmpq %rbx, %rax
+   je else_ultimo_bloco
+   # ultimoBloco  = topoInicialHeap + 1;
+   movq TOPO_INICIAL_HEAP, %rax
+   addq $1, %rax
+   movq %rax, -16(%rbp)
+   # percorreHeap = ultimoBloco + *(ultimoBloco + 8) + 16;
+   movq -16(%rbp), %rax
+   movq %rax, %rbx
+   addq $8, %rbx
+   movq (%rbx), %rbx
+   addq $16, %rax
+   addq %rbx, %rax
+   movq %rax, -8(%rbp)
+   # while (percorreHeap < topoAtualHeap)
+while_ultimo_bloco:
+   movq -8(%rbp), %rax
+   movq TOPO_ATUAL_HEAP, %rbx
+   cmpq %rbx, %rax
+   jge fim_if_ultimo_bloco
+   # ultimoBloco  = percorreHeap;
+   movq %rax, -16(%rbp)
+   # percorreHeap = ultimoBloco + *(ultimoBloco + 8) + 16;
+   movq -16(%rbp), %rax
+   movq %rax, %rbx
+   addq $8, %rbx
+   movq (%rbx), %rbx
+   addq $16, %rax
+   addq %rbx, %rax
+   movq %rax, -8(%rbp)
+   jmp while_ultimo_bloco
+else_ultimo_bloco:
+   # ultimoBloco = topoAtualHeap + 1;
+   movq TOPO_ATUAL_HEAP, %rax
+   addq $1, %rax
+   movq %rax, -16(%rbp)
+fim_if_ultimo_bloco:
+   # num_blocos = (ultimoBloco + 4111 + num_bytes - topoAtualHeap) / 4096;
+   movq -16(%rbp), %rax
+   addq $4111, %rax
+   movq 16(%rbp), %rbx
+   addq %rbx, %rax
+   movq TOPO_ATUAL_HEAP, %rbx
+   subq %rbx, %rax
+   shr $12, %rax
+   movq %rax, -32(%rbp)
+   # brk(topoAtualHeap + num_blocos * 4096);
+   shl $12, %rax
+   movq TOPO_ATUAL_HEAP, %rbx
+   addq %rbx, %rax
+   movq %rax, %rdi
+   movq $12, %rax
+   syscall
+   # topoAtualHeap = sbrk(0);
+   movq %rax, TOPO_ATUAL_HEAP
+   # *ultimoBloco = 1;
+   movq -16(%rbp), %rax
+   movq $1, (%rax)
+   # *(ultimoBloco + 8) = num_bytes;
+   movq 16(%rbp), %rax
+   movq -16(%rbp), %rbx
+   addq $8, %rbx
+   movq %rax, (%rbx)
+   # *(ultimoBloco + 16 + num_bytes) = 0;
+   movq -16(%rbp), %rax
+   movq 16(%rbp), %rbx
+   addq $16, %rax
+   addq %rbx, %rax
+   movq $0, (%rax)
+   # *(ultimoBloco + 24 + num_bytes) = 
+   # topoAtualHeap + 1 - (ultimoBloco + 32 + num_bytes);
+   movq -16(%rbp), %rbx
+   movq 16(%rbp), %rax
+   addq $32, %rbx
+   addq %rax, %rbx
+   movq TOPO_ATUAL_HEAP, %rax
+   addq $1, %rax
+   subq %rbx, %rax
+   movq -16(%rbp), %rbx
+   addq $24, %rbx
+   movq 16(%rbp), %r10
+   addq %r10, %rbx
+   movq %rax, (%rbx)
+   # melhorBloco = ultimoBloco;
+   movq -16(%rbp), %rax
+   movq %rax, -24(%rbp)
+fim_if_melhor_bloco:
+   # return (melhorBloco + 16);
+   movq -24(%rbp), %rax
+   addq $16, %rax
    # - - - - - - - - - - - - - -
    # Fim da Função 
    addq $32, %rsp                  
@@ -276,7 +464,6 @@ fora_while:
    # printf("\n");
    movq STR_NOVA_LINHA, %rdi    
    call printf
-
    # - - - - - - - - - - - - - -
    # Fim da Função              
    addq $24, %rsp                  
